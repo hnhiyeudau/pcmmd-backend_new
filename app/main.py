e5900244d4e76bef92d2e73d64d2b71a3c412e30
hnhiyeudau <86763744+hnhiyeudau@users.noreply.github.com>
2025-05-13 20:18:33 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-print("✅ CORS middleware loaded with:", allow_origins)
+# print("✅ CORS middleware loaded with:", allow_origins)
 
 # Root endpoint kiểm tra server
 @app.get("/")
